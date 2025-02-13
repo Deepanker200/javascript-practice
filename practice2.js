a3ba0036f7f8151fc16c644a0b093a66236cb576
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-13 23:51:08 +0530

    Examples of DataTypes in JS

--- practice2.js
+++ practice2.js
@@ -6,13 +6,51 @@
 // console.log(typeof undefined)
 
 
-let a="33abv";
+// let a = "33abv";
 
-let b=Number(a);
+// let b = Number(a);
 
 // console.log(a+b);
 
-let c=NaN;
-console.log(b);         //NaN
-console.log(typeof b);  //number
-console.log(c);
+// let c=NaN;
+// console.log(b);         //NaN
+// console.log(typeof b);  //number
+// console.log(c);
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + 2 + "2" + 4 + 9);
+console.log(1 + "2" + 2);
+console.log(3 + 4 * 5 % 3);
+
+console.log(true);
+console.log(+true);
+// console.log(true+);      //Will not run
+console.log(+"");
+// console.log(""+);
+
+
+//Post Fix and Pre Fix
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
