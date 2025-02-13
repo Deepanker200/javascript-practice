6f39df74752ad4742967b974572a693d1d90bf52
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-13 23:35:05 +0530

    JS DataTypes continue..

---
+++ practice2.js
@@ -0,0 +1,18 @@
+// "use strict";
+// console.log(3 +3)
+
+// console.log(typeof"Deepanker")
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+
+let a="33abv";
+
+let b=Number(a);
+
+// console.log(a+b);
+
+let c=NaN;
+console.log(b);         //NaN
+console.log(typeof b);  //number
+console.log(c);
