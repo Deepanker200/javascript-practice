d6491b6a2019ba36328e04677ff15c5ea86f90df
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-16 00:26:39 +0530

    Strings in JS

--- string.js
+++ string.js
@@ -1,4 +1,22 @@
-const name="Deepanker";
-const rank=1;
+const name = "Deepanker";
+const rank = 1;
 
-console.log(name+rank);
+console.log(name + rank);
+
+console.log(`This is my ${rank}`);
+console.log(name[0]);
+
+console.log(name.charAt(2));
+console.log(name.indexOf('e'));
+console.log(name.indexOf(`e`));
+
+
+const newString = name.substring(1, 4);
+
+console.log(newString);
+
+const anotherString=name.slice(-8,4);
+const anotherString2=name.slice(-18,4);
+
+console.log(anotherString);
+console.log(anotherString2);
