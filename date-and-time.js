3c4ed6314da0fd783b1f5c26e756cf10d46c7d9f
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-18 23:36:23 +0530

    Date and Time

---
+++ date-and-time.js
@@ -0,0 +1,18 @@
+//Date is long stand pain point in JS
+
+let myDate= new Date();
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+
+
+let myTimeStamp=Date.now();
+
+console.log(myTimeStamp);
+console.log(myDate.getMonth())
+console.log(myDate.getDate())
+console.log(myDate.getDay())
