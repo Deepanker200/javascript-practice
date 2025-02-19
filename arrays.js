a6ec34bd92543f20429a6444ad43bc68853298a0
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-19 23:52:05 +0530

    Array Part 1

---
+++ arrays.js
@@ -0,0 +1,23 @@
+//arrays in js
+
+const arr=[1,2,3,4,5];
+const arr2=new Array("G",1,2,"4","hello");
+console.log(arr);
+console.log(arr2);
+console.log(typeof arr2[3]);
+
+
+arr.push(6);
+
+arr.pop();          //No value to be inserted
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+arr.unshift(10);
+
+console.log(arr);
+
+//slice and splice
+
