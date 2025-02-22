bd8d00cb7ce84260f51927348acf7c430eb4fca8
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-23 00:05:24 +0530

    Object de-sturcture and json

--- json-formatter.js
+++ json-formatter.js
@@ -1,11 +1,33 @@
-const course={
+const course = {
     coursename: "JS in Hindi",
-    price:"999",
-    courseInstructor: "Hitesh"
+    price: "999",
+    courseInstructor: "Deepanker"
 }
 
 //course.courseInstructor
 
-const{courseInstructor}=course
+const { courseInstructor: instructor } = course
 
-console.log(courseInstructor);
+// console.log(courseInstructor);
+
+console.log(instructor);
+
+// const navbar=({company})=>{
+
+// }
+
+// navbar(company="Deepanker")
+
+
+// {
+//     name:"Deepanker",
+//     "coursename":"JS in Hindi",
+//     "price":"free"
+// }
+
+
+[
+    {},
+    {},
+    {},
+]
