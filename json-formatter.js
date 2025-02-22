26e0b414a64f4bad983ef48d2209c12abc13fe0e
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-22 23:51:37 +0530

    Object destructure

---
+++ json-formatter.js
@@ -0,0 +1,11 @@
+const course={
+    coursename: "JS in Hindi",
+    price:"999",
+    courseInstructor: "Hitesh"
+}
+
+//course.courseInstructor
+
+const{courseInstructor}=course
+
+console.log(courseInstructor);
