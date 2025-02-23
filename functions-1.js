fdddda86eab319356318342fe2194514f3323d59
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-23 21:24:22 +0530

    Funtions Part 2

--- functions-1.js
+++ functions-1.js
@@ -1,15 +1,15 @@
-function syaMyName() {
-    console.log("D");
-    console.log("E");
-    console.log("E");
-    console.log("P");
-    console.log("A");
-    console.log("N");
-    console.log("K");
-    console.log("E");
-    console.log("R");
+// function syaMyName() {
+//     console.log("D");
+//     console.log("E");
+//     console.log("E");
+//     console.log("P");
+//     console.log("A");
+//     console.log("N");
+//     console.log("K");
+//     console.log("E");
+//     console.log("R");
 
-}
+// }
 
 // syaMyName();
 
@@ -24,19 +24,44 @@ function syaMyName() {
 // addToNumbers(2,"a");
 
 
-function addToNumbers(n1, n2) {
-    return(n1 + n2);
+// function addToNumbers(n1, n2) {
+//     return(n1 + n2);
+// }
+
+// const result = addToNumbers(3, 6);
+// console.log("Result:", result);
+
+// function login(username="Sam"){
+//     if(username===undefined){
+//         console.log("Please enter correct username")
+//         return  //this return is sending undefined
+//     }
+//     return username
+// }
+
+// console.log(login());
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1;
 }
 
-const result = addToNumbers(3, 6);
-console.log("Result:", result);
+console.log(calculateCartPrice(21,24,2));
 
-function login(username="Sam"){
-    if(username===undefined){
-        console.log("Please enter correct username")
-        return  //this return is sending undefined
-    }
-    return username
+const user={
+    username:"Deepanker",
+    price:199
 }
 
-console.log(login());
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject['price']}`);
+}
+
+handleObject(user);
+
+const myNewArray=[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[2];
+}
+
+console.log(returnSecondValue(myNewArray));
