b43dcd8ccbf13b962f785af89c4419ab62da4048
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-23 23:06:47 +0530

    Global-Local Scope

---
+++ global-local.js
@@ -0,0 +1,56 @@
+// var c=300
+// let a=400
+// if(true){
+//     let a=10;
+//     const b=20;
+//     console.log("Inner: ",a);
+//     // var c=30;
+
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one() {
+    var username = "Deepanker";
+
+    function two() {
+        const website = "freakcoder";
+        console.log(username);
+    }
+
+    // console.log(website);
+    two();
+}
+
+one();
+// console.log(username);
+
+if (true) {
+    const username = "shubham";
+    if ("shubham"===username) {
+        const website = "youtube";
+        console.log(username+website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++ interesting +++++++++
+
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+
+
+
+//Expression
+const addTwo=function(num){
+    return num+2;
+}
+addTwo(5);
