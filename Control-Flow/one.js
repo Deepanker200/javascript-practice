c190080afa3f6efc299302df0d33f64b1a778f15
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-26 23:42:11 +0530

    Control Flow continue..

--- Control-Flow/one.js
+++ Control-Flow/one.js
@@ -1,7 +1,31 @@
 //if
 
-const isUserloggedIn=true
+const isUserloggedIn = true
+const temp = 41
+// if(2==="2"){
+//     console.log("Executed");
+// }
 
-if(2==="2"){
-    console.log("Executed");
+if (temp === 41) {
+    console.log("Less than 50")
+}
+console.log("Temperature is greater than 50");
+
+const balance=800;
+
+// if(balance>500) console.log("Test");
+
+if (balance<500){
+    console.log("Less than 500");
+}
+else if (balance<700){
+    console.log("Less than 700");
+}
+
+else if (balance<900){
+    console.log("Less than 900");
+}
+
+else{
+    console.log("Less than 1200")
 }
