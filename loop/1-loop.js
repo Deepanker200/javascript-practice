32f06bec459e9d92f8dee01bbe3d4e96aa84f4cd
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-27 23:45:05 +0530

    for loop

---
+++ loop/1-loop.js
@@ -0,0 +1,23 @@
+//for loop
+
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     if (element == 5) {
+//         console.log("5 is best");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     for(let i=0;i<=10;i++){
+
+//     }
+//     const element = array[index];
+// }
+
+myArray=["Deepanker","Lakshay","Arpit","Ashutosh"];
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
