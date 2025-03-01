5870a3a5a50d7a476bbf09139fdcb9976711e69e
Deepanker Tiwari <deepamamazing@gmail.com>
2025-03-01 23:54:57 +0530

    Higher Order Array Loops

---
+++ loop/higher-order-loops.js
@@ -0,0 +1,46 @@
+// const arr=[1,2,3,4,5];
+
+// for (const num of arr) {
+//     console.log(num);
+
+// }
+
+// const greetings="Hello in the world of JS";
+
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+// const map = new Map()        //map is not iterable
+
+// map.set('IN', "India");
+// map.set('USA', "America");
+// map.set('UK', "United Kingdom");
+// map.set('Australia', "Australia");
+// map.set('IN', "India");
+
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,':-',value);
+// }
+
+// const myObject = {
+//     js: "javascript",
+//     cpp: "C++",
+//     rb:'ruby',
+//     py:"python"
+// }
+
+
+
+// for (const key in myObject) {
+//    console.log(key,':-',myObject[key]);
+// }
+
+
+const programming=["js","python","rust","php","cpp"];
+
+for(const key in programming){
+    console.log(key);
+}
---
+++ loop/higher-order-loops2.js
@@ -0,0 +1,43 @@
+const coding = ["js", "python", "ruby", "java", "cpp", "php"];
+
+
+// coding.forEach(
+//     function (item) {
+//         console.log(item);
+
+//     }
+// )
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+})
+
+myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "mysql",
+        languageFileName: "sql"
+    },
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageFileName);
+})
