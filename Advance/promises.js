c51a7a912833bd543c313cc134fc63b052594d2e
Deepanker Tiwari <deepamamazing@gmail.com>
2025-03-12 23:48:23 +0530

    Promise continue..

--- Advance/promises.js
+++ Advance/promises.js
@@ -32,5 +32,37 @@ const promiseThree = new Promise(function (resolve, reject) {
 
 promiseThree.then(function (user) {
     console.log(user);
+})
 
+const promiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true
+        if (!error) {
+            resolve({ username: "Deepanker", password: "123" })
+        } else {
+            reject("ERROR: Something went wrong")
+        }
+    }, 1000)
+})
+
+promiseFour
+    .then((user) => {
+        console.log(user);
+        return user.username
+    }).then((username) => {
+        console.log(username);
+    }).catch(function (error) {
+        console.log(error);
+    }).finally(() => console.log("The promise is either resoled or rejected"))
+
+
+const promiseFive = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true
+        if (!error) {
+            resolve({ username: "Deepanker", password: "123" })
+        } else {
+            reject("ERROR: JS went wrong")
+        }
+    }, 1000)
 })
