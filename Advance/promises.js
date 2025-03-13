d1870c66351656a596b45901dbcac553e26425aa
Deepanker Tiwari <deepamamazing@gmail.com>
2025-03-13 23:56:59 +0530

    Promises and async-await continue..

--- Advance/promises.js
+++ Advance/promises.js
@@ -66,3 +66,38 @@ const promiseFive = new Promise(function (resolve, reject) {
         }
     }, 1000)
 })
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://api.github.com/users/Deepanker200')
+//         const data = await response.json()
+//         console.log(data);
+
+//     } catch (error) {
+//         console.log("E: ", error);
+
+//     }
+// }
+
+// getAllUsers()
+
+
+fetch('https://api.github.com/users/Deepanker200')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>console.log(error))
