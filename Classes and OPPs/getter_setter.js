5a8464659d9d94c157c53b403b753df27588a882
Deepanker Tiwari <deepamamazing@gmail.com>
2025-03-16 13:15:26 +0530

    JS Basics completed

---
+++ Classes and OPPs/getter_setter.js
@@ -0,0 +1,28 @@
+class User{
+    constructor(email,password){
+        this.email = email;
+        this.password = password;
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email=value
+    }
+
+    get password(){
+        return `${this._password}xyz`
+    }
+
+    set password(value){
+        this._password=value
+    }
+}
+
+
+const deep=new User('tiwarideepanker@gmail.com',"addss")
+
+console.log(deep.password);
+console.log(deep.email);
---
+++ Classes and OPPs/mathpi.js
@@ -0,0 +1,11 @@
+const descripter=Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(descripter);
+
+
+const m1={
+    name:"Tea",
+    price:30,
+    isAvailable:true
+}
+
+console.log(Object.getOwnPropertyDescriptor(m1,"name"));
