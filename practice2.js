6110581edebd79a06f17e1308a5f8fef917b6af9
Deepanker Tiwari <deepamamazing@gmail.com>
2025-03-19 23:07:10 +0530

    JS Revision-1

--- practice2.js
+++ practice2.js
@@ -6,17 +6,20 @@
 // console.log(typeof undefined)
 
 
-let a = "33abv";
+// let a = "33abv";
+// console.log(typeof a);
 
-let b = Number(a);
+// let b = Number(a);
 
-console.log(a+b);
+// console.log(a+b);
 
 // let c=NaN;
 // console.log(b);         //NaN
 // console.log(typeof b);  //number
-// console.log(c);
+// console.log(typeof c);
 
+let d="abcd2123"
+// console.log(typeof d)
 
 // console.log(2 + 2);
 // console.log(2 - 2);
@@ -25,21 +28,25 @@ console.log(a+b);
 // console.log(2 / 2);
 // console.log(2 % 2);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
-console.log(1 + 2 + "2" + 4 + 9);
-console.log(1 + "2" + 2);
-console.log(3 + 4 * 5 % 3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2" + 4 + 9);
+// console.log(1 + "2" + 2);
+// console.log(3 + 4 * 5 % 3);
 
-console.log(true);
-console.log(+true);
+// console.log(true);
+// console.log(+true);
 // console.log(true+);      //Will not run
-console.log(+"");
+// console.log(true+false);      //Will not run
+// console.log(+"");
 // console.log(""+);
 
+// let p=10;
+// console.log(++p);
+// console.log(p)
 
 //Post Fix and Pre Fix
 
@@ -49,8 +56,8 @@ const y = x++;
 console.log(`x:${x}, y:${y}`);
 // Expected output: "x:4, y:3"
 
-// let a = 3;
-// const b = ++a;
+let a = 3;
+const b = ++a;
 
-// console.log(`a:${a}, b:${b}`);
+console.log(`a:${a}, b:${b}`);
 // Expected output: "a:4, b:4"
