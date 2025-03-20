109199a8b98ba84eccf2c3cf5572d3b050946757
Deepanker Tiwari <deepamamazing@gmail.com>
2025-03-20 23:33:14 +0530

    JS Revision-2

--- practice3.js
+++ practice3.js
@@ -1,14 +1,23 @@
 // "use strict";
 // console.log(2>1);
-console.log("2" > 1);
-console.log("02" > 1);
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("0.2" > 1);
 
-console.log(null==0);
-console.log(null>0);
-console.log(null>=0);
-console.log(undefined==null);
-console.log(undefined==0);
-console.log(null==0);
+// console.log(null==0);
+// console.log(typeof null);
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(0>=null);
+
+// console.log(false>=0);
+// console.log(false<=null);
+
+
+// console.log(undefined==null);
+// console.log(undefined==0);
+// console.log(null==0);
 
 // "use strict";
 function sum(a, a) { 
@@ -22,3 +31,11 @@ function sum2(a, b) {
 
 console.log(sum(9,1));
 console.log(sum2(9,1));
+
+console.log(null==NaN);
+console.log("">=null);
+console.log(undefined=="");
+console.log(undefined<="");
+console.log(undefined>="");
+console.log(undefined==null);
+
--- practice4.js
+++ practice4.js
@@ -1,8 +1,14 @@
 const id1=Symbol('123');
 const id2=Symbol('123');
+console.log(id1)
+// console.log(big);
+
 let a=null;
 const big=231232131321323212413324324n;
 console.log(big);
 console.log(id1==id2);
 console.log(typeof a);
 console.log(typeof id1);
+let c="10"
+let d=10;
+console.log(c==d);
