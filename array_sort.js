bc6e0697d94d916b41e84c90915a81a84c79dc3b
Deepanker Tiwari <deepamamazing@gmail.com>
2025-03-23 19:36:21 +0530

    JS Revision-4

---
+++ array_sort.js
@@ -0,0 +1,6 @@
+arr=["aman","nitish","bobby","yash","sid"]
+console.log(arr.sort());
+
+
+arr2=[20,40,10,18,29,90,99,89,70]
+console.log(arr2.sort());
--- loop/for-loop.js
+++ loop/for-loop.js
@@ -16,8 +16,30 @@
 //     const element = array[index];
 // }
 
-myArray=["Deepanker","Lakshay","Arpit","Ashutosh"];
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    console.log(element);
+// myArray=["Deepanker","Lakshay","Arpit","Ashutosh"];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+// // for(let i=15;i>=1;i+=2){
+// //     console.log(i);
+// // }
+
+// for (let i=1;i<=3;i++){
+//     // console.log(i);
+//     for(let j=1;j<=3;j++){
+//         console.log(j);
+//     }
+// }
+
+
+myArray=[["Deepanker","Lakshay"],["Arpit","Ashutosh"]];
+
+for(let i=0;i<myArray.length;i++){
+    console.log("List: ",myArray[i]);
+    for(let j=0;j<myArray[i].length;j++){
+        console.log("Name: ",myArray[i][j]);
+}
 }
