22ab3b53d9d8ca3a5813ecdc3ad8610ad6f8c8d8
Deepanker Tiwari <deepamamazing@gmail.com>
2025-04-10 23:54:00 +0530

    Async Await in JS

---
+++ Advance/imp-question.js
@@ -0,0 +1,12 @@
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "Deepanker", email: "tiwarideepanker@gmail.com" });
+    },
+        2000)
+})
+
+promiseThree.then(function (user) {
+    
+    console.log(user);
+    console.log("hello");
+})
