1976c5c08867721ab75162378f4fa4fd51e211ff
Deepanker Tiwari <deepamamazing@gmail.com>
2025-04-14 23:29:43 +0530

    this keyword

--- Advance/this.js
+++ Advance/this.js
@@ -5,7 +5,7 @@
 console.log(this);
 
 
-function x(){
+function x() {
     //the value depends on strict and non-strict mode
     console.log(this);
 }
@@ -22,3 +22,50 @@ x();
 //this keyword value depends on how the function is called
 window.x();
 
+
+
+const student = {
+    name: "Deepanker",
+    printName: function () {
+        // console.log(this);
+        console.log(this.name);
+    }
+}
+
+student.printName()
+
+const student2 = {
+    name: "Deepika"
+}
+
+student.printName.call(student2);   //value of this=student2
+
+//this inside arrow function
+
+const obj = {
+    a: 10,
+    x: () => {
+        console.log(this);
+    }
+}
+
+obj.x()
+
+
+const obj2 = {
+    a: 20,
+    x:function(){
+        //enclosing lexical context
+        const y=()=>{
+            console.log(this);
+        };
+        y()
+    },
+};
+
+obj2.x();
+
+
+//this inside DOM
+
+//this inside class and constructor
