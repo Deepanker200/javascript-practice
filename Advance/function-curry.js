1316626db2f24a8384c28bcbf9b3881c68b42ec9
Deepanker Tiwari <deepamamazing@gmail.com>
2025-04-16 21:51:27 +0530

    Currying in JS

---
+++ Advance/function-curry.js
@@ -0,0 +1,22 @@
+// let multiply=function (x,y){
+//     console.log(x*y)
+// }
+
+
+
+// let multiplyByTwo=multiply.bind(this,2,3);
+// multiplyByTwo(5)
+// let multiplyByThree=multiply.bind(this,3);
+
+// multiplyByThree(5)
+
+
+//Another Example of Function Currying
+let multiply=function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+
+let multiplyByTwo=multiply(2);
+multiplyByTwo(3)
