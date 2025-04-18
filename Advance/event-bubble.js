1ced2b0685e3b74484ec18fed44c49dad33af56a
Deepanker Tiwari <deepamamazing@gmail.com>
2025-04-18 23:14:23 +0530

    Event Delegation in JS

--- Advance/event-bubble.js
+++ Advance/event-bubble.js
@@ -12,3 +12,22 @@ document.querySelector("#child")
 .addEventListener('click',()=>{
     console.log("Child Clicked");
 },true);    //Event Capturing
+
+
+//Event Propagation
+
+// document.querySelector("#grandparent")
+// .addEventListener('click',()=>{
+//     console.log("Grandparent Clicked");
+// },false);        //Event capturing
+
+// document.querySelector("#parent")
+// .addEventListener('click',(e)=>{
+//     console.log("Parent Clicked");
+//     e.stopPropagation();
+// },false);   //Event Bubbling
+
+// document.querySelector("#child")
+// .addEventListener('click',()=>{
+//     console.log("Child Clicked");
+// },false);    //Event Capturing
