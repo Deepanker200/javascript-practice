359903a35d1f5d1424840f5df82a7377495f232e
Deepanker Tiwari <deepamamazing@gmail.com>
2025-09-26 23:35:18 +0530

    JS Practice-03~ Array revision

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@
 
 const arr=[1,2,3,4,5];
 const arr2=new Array("G",1,2,"4","hello");
-console.log(arr);
+console.log(typeof arr);    //object
 console.log(arr2);
 console.log(typeof arr2[3]);        //string
 
@@ -14,7 +14,7 @@ arr.pop();          //No value to be inserted
 console.log(arr);
 
 arr.shift();
-console.log(arr);
+console.log("arr.shift(): ", arr);
 
 arr.unshift(10);
 
@@ -23,20 +23,23 @@ console.log(arr);
 //slice and splice
 
 const arr3=[1,5,6,10,2,80]
-console.log(arr3.slice(1,5));
+console.log("Sliced array: ",arr3.slice(1,5));
 console.log(`Original array3: ${arr3}`);        //Doesn't change
 
 
 const arr4=[90,10,2,45,88,55,100]
-console.log(arr4.splice(1,5,2));  //returns [10,2] and modifies the array
+console.log("Spliced array: ",arr4.splice(1,5,2));  //returns [10,2] and modifies the array
 console.log(`Original array4: ${arr4}`);
 
 const arr5=[90,10,18,2,44,25];
-console.log(arr5.slice(-2,-5));
+console.log(arr5.slice(-2,-5));     //slice(start, end) only works if start < end (in terms of index).
+// console.log(arr5.slice(-5,-2));     
+//IMP: Negative deleteCount is ignored in Splice
 
 
 const arr6=["Mango","Grapes","Apple","Banana"];
 arr6[5]="Pineapple"
+// arr6[-5]="Pineapple"
 console.log(arr6);
 console.log(arr6[9]);
 
