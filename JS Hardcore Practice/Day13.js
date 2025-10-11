4233329db4ea6577002754c1419d8129aa11eb9b
Deepanker Tiwari <deepamamazing@gmail.com>
2025-10-11 07:43:54 +0530

    Deep copy in JS

--- JS Hardcore Practice/Day13.js
+++ JS Hardcore Practice/Day13.js
@@ -17,33 +17,57 @@
 
 // NEW
 
-var obj={
-    name:"Deep",
-    age:22,
-    social:{
-        facebook:{
-            ac1:"somewhere@gmail.com",
-            ac2:"somewhere@gmail.com",
+var obj = {
+    name: "Deep",
+    age: 22,
+    social: {
+        facebook: {
+            ac1: "somewhere@gmail.com",
+            ac2: "somewhere@gmail.com",
         },
-        twitter:{
-            free:{
-                ac1:"somewhere@gmail.com"
+        twitter: {
+            free: {
+                ac1: "somewhere@gmail.com"
             },
-            paid:{
-                ac1:"somewhere@gmail.com"
+            paid: {
+                ac1: "somewhere@gmail.com"
             }
         }
     }
 }
 
-var obj2={...obj}
+var obj2 = { ...obj }
 
-console.log(obj);
+// console.log(obj);
 // console.log(obj2);
+obj2.name = "Harish"  //not changed obj1
 
-obj2.social.facebook.ac1="changed"
-console.log(obj);
+// obj2.social.facebook.ac1="changed"
+// console.log("OBJ 1: ",obj);
 
-console.log(obj2);
+// console.log("OBJ 2: ",obj2);
 
 
+//Deep copy
+
+var obj3 = JSON.parse(JSON.stringify(obj))
+obj3.social.facebook.ac1 = "changed"
+console.log(obj.social.facebook.ac1);
+console.log(obj3.social.facebook.ac1);
+
+//Deep Copy Logic
+
+
+function makeDeepCopy(obj) {
+    if (typeof obj == 'object' || obj === null) {
+        return obj;
+    }
+    var copiedval = Array.isArray(obj) ? [] : {};
+    var keys = Object.keys(obj);
+    for (var i = 0; i < keys.length; i++) {
+        
+        copiedval[keys[i]] = makeDeepCopy(obj[keys[i]]);
+    }
+    return copiedval;
+}
+var copy = makeDeepCopy(obj);
