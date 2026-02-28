b1df188d59c02110c3a753a6f7df3366ec27d921
Deepanker Tiwari <deepamamazing@gmail.com>
2026-02-28 10:30:00 +0530

    JS Coding Questions

--- Coding Part/basic.js
+++ Coding Part/basic.js
@@ -30,17 +30,50 @@
 
 //5. Write a Program to use a callback function?
 
-function msg(name, callback) {
-    console.log(name);
-    callback()
+// function msg(name, callback) {
+//     console.log(name);
+//     callback()
+// }
+
+// function callback() {
+//     console.log("Callback called");
+// }
+
+// msg("Deepanker", callback)
+
+// const add = (a, b) => a + b;
+
+// console.log(add(10,2))
+
+//Fibonacci Series
+
+// const n = 10;
+// let num1 = 0, num2 = 1, nextnum;
+
+// function fib(no) {
+//     for (let i = 1; i <= no; i++) {
+//         console.log(num1);
+//         nextnum = num1 + num2;
+//         num1 = num2
+//         num2 = nextnum
+//     }
+// }
+
+// console.log(fib(n))
+
+//Program to find frequency
+
+const arr=[1,2,4,5,1,23,44,2,76,12,5]
+
+const freq={}
+
+for(i=0;i<arr.length;i++){
+    if(freq[arr[i]]){
+        freq[arr[i]]+=1
+    }
+    else{
+        freq[arr[i]]=1
+    }
 }
 
-function callback() {
-    console.log("Callback called");
-}
-
-msg("Deepanker", callback)
-
-const add = (a, b) => a + b;
-
-console.log(add(10,2))
+console.log(freq)
