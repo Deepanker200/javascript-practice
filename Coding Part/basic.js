66d4d6b84cc6945ae397ee91fa995f52983c9f1c
Deepanker Tiwari <deepamamazing@gmail.com>
2026-03-26 23:32:59 +0530

    JS Coding questions-1

---
+++ Coding Part/basic.js
@@ -0,0 +1,29 @@
+// 1. WAP to reverse a string
+
+// function rev(str1){
+    // return str1.split("").reverse().join("")
+    
+// }
+
+// console.log(rev("abc"))
+
+
+// 2. Largest in Array
+
+function largest(arr){
+    let max=arr[0];
+
+    for(let i=0;i<arr.length;i++){
+        if(max<arr[i]){
+            max=arr[i]
+        }
+    }
+
+    return max
+}
+
+const arr=[1,4,2,5,68,12,90,12,45];
+
+console.log(largest(arr));
+console.log(arr.unshift());
+
