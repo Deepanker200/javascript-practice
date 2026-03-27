f79ff0e930f3355d5f0a44f85b7c627292cd83e3
Deepanker Tiwari <deepamamazing@gmail.com>
2026-03-27 23:40:31 +0530

    JS Coding questions-2

--- Coding Part/basic.js
+++ Coding Part/basic.js
@@ -1,8 +1,8 @@
 // 1. WAP to reverse a string
 
 // function rev(str1){
-    // return str1.split("").reverse().join("")
-    
+// return str1.split("").reverse().join("")
+
 // }
 
 // console.log(rev("abc"))
@@ -10,20 +10,37 @@
 
 // 2. Largest in Array
 
-function largest(arr){
-    let max=arr[0];
+// function largest(arr){
+//     let max=arr[0];
 
-    for(let i=0;i<arr.length;i++){
-        if(max<arr[i]){
-            max=arr[i]
-        }
-    }
+//     for(let i=0;i<arr.length;i++){
+//         if(max<arr[i]){
+//             max=arr[i]
+//         }
+//     }
 
-    return max
+//     return max
+// }
+
+// const arr=[1,4,2,5,68,12,90,12,45];
+
+// console.log(largest(arr));
+// console.log(arr.shift());
+// console.log(arr);
+
+//5. Write a Program to use a callback function?
+
+function msg(name, callback) {
+    console.log(name);
+    callback()
 }
 
-const arr=[1,4,2,5,68,12,90,12,45];
+function callback() {
+    console.log("Callback called");
+}
 
-console.log(largest(arr));
-console.log(arr.unshift());
+msg("Deepanker", callback)
 
+const add = (a, b) => a + b;
+
+console.log(add(10,2))
